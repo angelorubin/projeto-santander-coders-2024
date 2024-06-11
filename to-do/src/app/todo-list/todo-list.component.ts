d240e9596d93fa85aaa2c66cb3ea4e142dce5aba
angelorubin <angelorubin@gmail.com>
2024-06-11 17:57:26 -0300

    Implementando todo-list

---
+++ to-do/src/app/todo-list/todo-list.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './todo-list.component.html',
+  styleUrl: './todo-list.component.css'
+})
+
+export class TodoListComponent {
+  name = 'angelo'
+  constructor() { }
+
+  sum(n1: string, n2: string) {
+    return n1 + n2
+  }
+}
