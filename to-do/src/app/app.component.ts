b7d2b3c3f921bb93977bb9683160a6d026375003
angelorubin <angelorubin@gmail.com>
2024-06-13 17:58:26 -0300

    Implementando home

--- to-do/src/app/app.component.ts
+++ to-do/src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { TodoListComponent } from './todo-list/todo-list.component';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TodoListComponent],
+  imports: [RouterOutlet, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ to-do/src/app/home/home.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { BannerComponent } from './banner/banner.component';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [BannerComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+
+export class HomeComponent {
+  imagePath: string = './';  // Defina o caminho da imagem
+}
