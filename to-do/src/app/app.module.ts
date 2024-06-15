f0c198e10ec493adbaed0b650acc856d6bc9a33b
angelorubin <angelorubin@gmail.com>
2024-06-15 18:41:12 -0300

    Implementado roteamento

--- to-do/src/app/app.module.ts
+++ to-do/src/app/app.module.ts
@@ -3,14 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { AboutModule } from './about/about.module';
 
 @NgModule({
   declarations: [AppComponent, HomeComponent],
-  imports: [
-    BrowserModule,
-    CommonModule
-  ],
+  imports: [BrowserModule, CommonModule, AboutModule],
   providers: [HomeComponent],
   bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
